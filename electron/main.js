934a82cfab3bf54da15cd20e89e28d2fa134d607
MickaelFM <faivrem@gmail.com>
2025-09-08 08:46:14 +0200

    Enhance Electron app window creation by adding platform-specific icons and improving app dock icon handling on macOS.

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,8 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, nativeImage } = require('electron');
 const path = require('path');
 
 function createWindow() {
-  const win = new BrowserWindow({
+  const windowOptions = {
     width: 1280,
     height: 800,
     webPreferences: {
@@ -10,11 +10,27 @@ function createWindow() {
       contextIsolation: true,
       nodeIntegration: false
     }
-  });
+  };
+
+  if (process.platform !== 'darwin') {
+    const iconFileName = process.platform === 'win32' ? 'icon.ico' : 'icon.png';
+    windowOptions.icon = path.join(__dirname, 'assets', iconFileName);
+  }
+
+  const win = new BrowserWindow(windowOptions);
   win.loadURL('http://localhost:3000');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  if (process.platform === 'darwin') {
+    const iconPath = path.join(__dirname, 'assets', 'icon.icns');
+    const icon = nativeImage.createFromPath(iconPath);
+    if (!icon.isEmpty()) {
+      app.dock.setIcon(icon);
+    }
+  }
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
