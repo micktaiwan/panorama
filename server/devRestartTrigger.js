3931bbe481779c10cd2288ede6e3149358454ab7
MickaelFM <faivrem@gmail.com>
2026-09-16 08:17:04 +0200

    chore: bump wake-recovery restart marker

--- server/devRestartTrigger.js
+++ server/devRestartTrigger.js
@@ -10,4 +10,4 @@
 // This file is imported by wakeRecovery.js only so it is part of the server
 // bundle and therefore part of the watch set. It intentionally exports nothing.
 // A "last wake-recovery restart" comment appended below is normal.
-// last wake-recovery restart: 2026-09-15T06:46:22.294Z
+// last wake-recovery restart: 2026-09-15T18:21:48Z
